fac935d7a8e7c9c877eabc3bcd3a55be1965b5bf
eneskucukov1 <eneskucukov@gmail.com>
2023-11-22 06:45:57 +0300

    Delete settings_example.py

--- python/settings_example.py
+++
@@ -1,4 +0,0 @@
-# WordPress configuration
-WP_XMLRPC_URL = '' # URL to WordPress XMLRPC file (http://www.test.com/xmlrpc.php)
-WP_USERNAME = '' # WordPress username
-WP_PASSWORD = '' # WordPress password
